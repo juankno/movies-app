bdfc58e7f1f47a0ed3fb38f83c104d0679b30681
Juan Esteban <juanes0202@gmail.com>
2022-04-20 09:40:25 -0500

    configurando el stack navigator

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -3,11 +3,23 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { HomeScreen } from '../screens/HomeScreen';
 import { DetailScreen } from '../screens/DetailScreen';
 
-const Stack = createStackNavigator();
+export type RootStackParams = {
+    HomeScreen: undefined,
+    DetailScreen: undefined,
+}
+
+const Stack = createStackNavigator<RootStackParams>();
 
 export const Navigation = () => {
     return (
-        <Stack.Navigator>
+        <Stack.Navigator
+            screenOptions={{
+                headerShown: false,
+                cardStyle: {
+                    backgroundColor: 'white',
+                },
+            }}
+        >
             <Stack.Screen name="HomeScreen" component={HomeScreen} />
             <Stack.Screen name="DetailScreen" component={DetailScreen} />
         </Stack.Navigator>
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,23 @@
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
+import { RootStackParams } from '../navigation/Navigation';
+
+type HomeScreenProps = StackNavigationProp<RootStackParams, 'HomeScreen'>;
 
 export const HomeScreen = () => {
+
+  const navigation = useNavigation<HomeScreenProps>();
+
   return (
     <View>
-        <Text>HomeScreen</Text>
+      <Text>HomeScreen</Text>
+
+      <Button
+        title="Ir a detalle"
+        onPress={() => navigation.navigate('DetailScreen')}
+      />
     </View>
   );
 };

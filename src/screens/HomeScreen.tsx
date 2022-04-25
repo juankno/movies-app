7c827ed2aad01406f89a8596da209002cd43b92a
Juan Esteban <juanes0202@gmail.com>
2022-04-25 18:00:39 -0500

    obteniendo los colores primarios

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { View, ActivityIndicator, Dimensions, ScrollView } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import ImageColors from 'react-native-image-colors';
 import Carousel from 'react-native-snap-carousel';
 
 import { useMovies } from '../hooks/useMovies';
@@ -9,6 +8,7 @@ import { styles } from '../theme/appTheme';
 import { CardMovie } from '../components/CardMovie';
 import { HorizontalSlider } from '../components/HorizontalSlider';
 import { GradientBackground } from '../components/GradientBackground';
+import { getImageColors } from '../helpers/getColors';
 
 const { width } = Dimensions.get('window');
 
@@ -22,9 +22,9 @@ export const HomeScreen = () => {
     const movie = nowPlaying[index];
     const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
 
-    const colors = await ImageColors.getColors(uri, {});
+    const [ primary, secondary ] = await getImageColors(uri);
 
-    console.log(colors);
+    console.log(primary, secondary);
 
   };
 

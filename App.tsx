897257a601888d184a3d603f479f032063b6e50b
Juan Esteban <juanes0202@gmail.com>
2022-04-26 17:19:04 -0500

    encapsulando la app en el gradiantProvider para menejar un estado global

--- App.tsx
+++ App.tsx
@@ -3,11 +3,32 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Navigation } from './src/navigation/Navigation';
 import { FadeScreen } from './src/screens/FadeScreen';
+import { GradientProvider } from './src/context/GradientContext';
+
+
+interface AppStateProps {
+  children: JSX.Element | JSX.Element[];
+}
+
+
+const AppState = ({ children }: AppStateProps) => {
+  return (
+    <GradientProvider>
+      {children}
+    </GradientProvider>
+  );
+};
 
 export const App = () => {
   return (
     <NavigationContainer>
-      <Navigation />
+
+      <AppState>
+
+        <Navigation />
+
+      </AppState>
+
     </NavigationContainer>
   );
 };
---
+++ src/context/GradientContext.tsx
@@ -0,0 +1,48 @@
+import React, { createContext, useState } from 'react';
+
+interface ImageColors {
+    primary: string;
+    secondary: string;
+}
+
+
+interface ContextProps {
+    colors: ImageColors;
+    prevColors: ImageColors;
+    setMainColors: (mainPrevColors: ImageColors) => void;
+    setPrevMainColors: (mainPrevColors: ImageColors) => void;
+}
+
+export const GradientContext = createContext({} as ContextProps); // TODO: definir tipo
+
+export const GradientProvider = ({ children }: any) => {
+
+    const [colors, setColors] = useState<ImageColors>({
+        primary: 'transparent',
+        secondary: 'transparent',
+    });
+
+    const [prevColors, setPrevColors] = useState<ImageColors>({
+        primary: 'transparent',
+        secondary: 'transparent',
+    });
+
+    const setMainColors = (mainColors: ImageColors) => {
+        setColors(mainColors);
+    };
+
+    const setPrevMainColors = (mainPrevColors: ImageColors) => {
+        setPrevColors(mainPrevColors);
+    };
+
+    return (
+        <GradientContext.Provider value={{
+            colors,
+            prevColors,
+            setMainColors,
+            setPrevMainColors,
+        }} >
+            {children}
+        </GradientContext.Provider>
+    );
+};
